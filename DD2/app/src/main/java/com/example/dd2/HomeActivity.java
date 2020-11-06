c94cc4918c90e1d1deaa860f349e93e783d7bc3a
Killawog <pauljohn2802@gmail.com>
2020-11-06 23:53:19 +0530

    Button functions for HomeActivity

--- DD2/app/src/main/java/com/example/dd2/HomeActivity.java
+++ DD2/app/src/main/java/com/example/dd2/HomeActivity.java
@@ -11,6 +11,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
     Button btnLogout;
+    Button btn,btn2,btn3,btn4,btn5;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
 
@@ -28,6 +29,61 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intToMain = new Intent(HomeActivity.this, MainActivity.class );
+                startActivity(intToMain);
+
+            }
+        });
+
+        btn=findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToMain = new Intent(HomeActivity.this, NGO_List.class );
+                startActivity(intToMain);
+
+            }
+        });
+
+        btn2=findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToMain = new Intent(HomeActivity.this, NGO_List.class );
+                startActivity(intToMain);
+
+            }
+        });
+
+        btn3=findViewById(R.id.button3);
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToMain = new Intent(HomeActivity.this, NGO_List.class );
+                startActivity(intToMain);
+
+            }
+        });
+
+        btn4=findViewById(R.id.button4);
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToMain = new Intent(HomeActivity.this, NGO_List.class );
+                startActivity(intToMain);
+
+            }
+        });
+
+        btn5=findViewById(R.id.button5);
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intToMain = new Intent(HomeActivity.this, NGO_List.class );
                 startActivity(intToMain);
 
             }
---
+++ DD2/app/src/main/java/com/example/dd2/NGO_List.java
@@ -0,0 +1,14 @@
+package com.example.dd2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NGO_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_n_g_o__list2);
+    }
+}
